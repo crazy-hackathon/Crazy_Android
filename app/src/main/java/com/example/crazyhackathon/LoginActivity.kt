8c8efa4e1343c1b19460b46d3996a5e1775bebc4
bhindor <bhindor@dgsw.hs.kr>
2022-11-03 10:24:11 +0900

    Modify : Login 비밀번호 가리기 추가

--- app/src/main/java/com/example/crazyhackathon/LoginActivity.kt
+++ app/src/main/java/com/example/crazyhackathon/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.crazyhackathon
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.view.View
 import com.example.crazyhackathon.databinding.ActivityLoginBinding
 
@@ -15,12 +16,14 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.etPw.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
         settingListener()
     }
 
     private fun settingListener() {
         binding.btnLogin.setOnClickListener(this)
         binding.btnJoin.setOnClickListener(this)
+        binding.imgPwView.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -35,6 +38,20 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                 Intent(this, JoinActivity::class.java).run {
                     startActivity(this)
                 }
+            }
+            binding.imgPwView -> {
+                if (binding.imgPwView.tag.equals("0")) {
+                    binding.imgPwView.tag = "1"
+                    binding.imgPwView.setImageResource(R.drawable.open)
+
+                    binding.etPw.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                } else {
+                    binding.imgPwView.tag = "0"
+                    binding.imgPwView.setImageResource(R.drawable.close)
+
+                    binding.etPw.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                }
+                binding.etPw.setSelection(binding.etPw.text.length)
             }
         }
     }
