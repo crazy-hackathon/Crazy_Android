d80daeff871076bc97c2bce58c62ef1496ac91ed
bhindor <bhindor@dgsw.hs.kr>
2022-11-03 15:02:16 +0900

    Add : main화면, main fragment

--- app/src/main/java/com/example/crazyhackathon/MainActivity.kt
+++ app/src/main/java/com/example/crazyhackathon/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.crazyhackathon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import com.example.crazyhackathon.databinding.ActivityMainBinding
+import com.example.crazyhackathon.fragment.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = findViewById<View>(R.id.bottom_navigation) as BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        supportFragmentManager.beginTransaction().replace(binding.bodyLayout.id, HomeFragment()).commit()
+        binding.bottomNavigation.selectedItemId = R.id.page_home
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.page_search -> {
+
+            }
+            R.id.page_home -> {
+                supportFragmentManager.beginTransaction().replace(binding.bodyLayout.id, HomeFragment()).commit()
+            }
+            R.id.page_my -> {
+
+            }
+        }
+        return true
     }
 }
