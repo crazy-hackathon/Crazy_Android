d8ebf830499734ad1c2ef6e8963d6850c0fed4f4
bhindor <bhindor@dgsw.hs.kr>
2022-11-04 12:35:07 +0900

    Delete : UserData 삭제

--- app/src/main/java/com/example/crazyhackathon/data/UserData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.crazyhackathon.data
-
-data class UserData(
-    var name: String,
-    var id: String,
-    var pw: String,
-    var gender: String,
-    var age: Int
-): java.io.Serializable
