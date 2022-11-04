db8a5b6a3bc1f84cb0fc97de4231c97031212077
bhindor <bhindor@dgsw.hs.kr>
2022-11-05 02:13:47 +0900

    Modify : .

--- app/src/main/java/com/example/crazyhackathon/adapter/PostRecyclerAdapter.kt
+++ app/src/main/java/com/example/crazyhackathon/adapter/PostRecyclerAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.crazyhackathon.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.crazyhackathon.DetailActivity
 import com.example.crazyhackathon.R
 import com.example.crazyhackathon.data.PostData
 import com.example.crazyhackathon.databinding.PostListBinding
@@ -21,6 +23,14 @@ class PostRecyclerAdapter (val context: Context):
             binding.tvLocation.text = data.location
             binding.tvHeart.text = data.heart.toString() + "명이 좋아합니다"
             binding.tvClock.text = data.time.toString()
+            binding.postLayout.setOnClickListener {
+                Intent(context, DetailActivity::class.java).apply {
+                    putExtra("title", data.title)
+                    putExtra("head", data.time)
+                    putExtra("body", data.location)
+                    putExtra("heart", data.heart)
+                }.run { context.startActivity(this) }
+            }
         }
     }
 
