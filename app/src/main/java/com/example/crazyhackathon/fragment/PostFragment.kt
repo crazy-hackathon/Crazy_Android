72f693cabdef1f9e6bfe1d9967cf2cdd7486d767
bhindor <bhindor@dgsw.hs.kr>
2022-11-04 19:10:12 +0900

    Modify : post upload 잇기

--- app/src/main/java/com/example/crazyhackathon/fragment/PostFragment.kt
+++ app/src/main/java/com/example/crazyhackathon/fragment/PostFragment.kt
@@ -1,5 +1,6 @@
 package com.example.crazyhackathon.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.crazyhackathon.R
+import com.example.crazyhackathon.UploadActivity
 import com.example.crazyhackathon.adapter.PlaceRecyclerAdapter
 import com.example.crazyhackathon.adapter.PostRecyclerAdapter
 import com.example.crazyhackathon.data.PostData
@@ -32,6 +34,11 @@ class PostFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initDataRecyclerView()
         addDataRecyclerView()
+        binding.btnFloating.setOnClickListener {
+            Intent(context, UploadActivity::class.java).run {
+                startActivity(this)
+            }
+        }
     }
 
     private fun initDataRecyclerView() {
